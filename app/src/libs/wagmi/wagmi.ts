b67995d7161836457d840cf4c97a54f1b330803f
HirokiYasui <hiromaily2@gmail.com>
2023-05-15 14:18:27 +0900

    add wagmi mock

--- app/src/libs/wagmi/wagmi.ts
+++ app/src/libs/wagmi/wagmi.ts
@@ -4,6 +4,8 @@ import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { MockConnector } from 'wagmi/connectors/mock'
+import { Wallet } from 'ethers'
 
 import { getChains } from './chains'
 import { getProviders } from './providers'
@@ -30,31 +32,52 @@ const { chains, provider, webSocketProvider } = configureChains(
 // https://github.com/pancakeswap/pancake-frontend/blob/912eee4ceec53ebba03edf4acbd0276f29095b96/apps/web/src/utils/wagmi.ts#L7
 // https://github.com/sushiswap/sushiswap/blob/13d5644ed4501ad45b0b4771f21555583e5238c6/jobs/pool/src/lib/wagmi.ts#L1
 
+// create signer
+const signer = Wallet.createRandom()
+
+let connectors = [
+  new MetaMaskConnector({ chains }),
+  new CoinbaseWalletConnector({
+    chains,
+    options: {
+      appName: config.appName,
+    },
+  }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      projectId: '...',
+    },
+  }),
+  new InjectedConnector({
+    chains,
+    options: {
+      name: 'Injected',
+      shimDisconnect: true,
+    },
+  }),
+  new MockConnector({
+    chains,
+    options: {signer: signer},
+  }),
+];
+
+// add mock
+// if (process.env.NODE_ENV === 'development'){
+//   // create signer
+//   const signer = Wallet.createRandom()
+
+//   const mockConn = new MockConnector({
+//     chains,
+//     options: {signer: signer},
+//   })
+//   connectors.push(mockConn)
+// }
+
 // client
 export const client = createClient({
   autoConnect: false,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: config.appName,
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: '...',
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'Injected',
-        shimDisconnect: true,
-      },
-    }),
-  ],
+  connectors: connectors,
   provider,
   //webSocketProvider,
 })
